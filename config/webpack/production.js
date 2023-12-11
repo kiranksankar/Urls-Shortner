46e7ee1c064afb70265853df3ee6eee4fa4589e4
Kiran K S <kiran.ks@geckolyst.com>
2023-12-11 17:20:31 +0530

    packs added

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,18 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+module.exports = {
+    mode: 'production',
+    entry: {
+      main: 'packs', 
+      // Example single entry point
+      // If you have multiple entry points, you can define them like this:
+    //   admin: './src/admin.js',
+    //   dashboard: ['./src/dashboard.js', './src/dashboardStyles.css'],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+      },
+      // 
+}
