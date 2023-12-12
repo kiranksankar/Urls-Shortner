2fa395f021565b1134fdad623cc9d82c4c947f13
Kiran K S <kiran.ks@geckolyst.com>
2023-12-12 15:04:51 +0530

    ll

--- db/migrate/20231121104213_users.rb
+++ db/migrate/20231121104213_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :username
       t.string :email
       t.string :password_digest
+      t.string :age 
       t.timestamps
     end
   end
