188652ac1ca09feac547929c8d1c2b1a9d40b1c7
Kiran K S <kiran.ks@geckolyst.com>
2023-12-12 14:33:20 +0530

    removed components

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
+// const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+// module.exports = environment.toWebpackConfig()
+module.exports = {
+    mode: 'production',
+    entry: './app/javascript/application.js', // Example entry path pointing to 'index.js' in the 'src' directory
+    // Other webpack configuration options...
+  };
