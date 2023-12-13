3c08e1d5f46cdf3cacb4a10d8f0bca0dd2cc5079
Kiran K S <kiran.ks@geckolyst.com>
2023-12-13 12:01:40 +0530

    webpack update

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,28 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+// process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const environment = require('./environment')
+// const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  node: {
+    __dirname: false, // Controls whether __dirname should be available
+    __filename: false, // Controls whether __filename should be available
+    global: true, // Controls whether the 'global' variable should be available
+  },
+  entry: {
+    
+    // main: '.app/javascript/application.js',
+  },
+  output: {
+    // Output configuration
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  // Other webpack configurations specific to production...
+};
+  
+
+// module.exports = environment.toWebpackConfig()
