92a36e21ee7a8662d5832128a37ce9ef76620ae2
Kiran K S <kiran.ks@geckolyst.com>
2023-12-18 15:54:33 +0530

    eamil unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+
   has_secure_password
 
   validates :username, presence: true, length: { maximum: 50 }
 
 
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
 
 
   validates :password, presence: true, length: { minimum: 6 }
