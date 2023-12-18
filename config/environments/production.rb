e798c78642c58092dec65e5456f9012ff74a19a3
Kiran K S <kiran.ks@geckolyst.com>
2023-12-18 15:49:31 +0530

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,23 @@ config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: 'ksgeckoklyst@gmail.com', # Replace with your Gmail address
+  password: 'rqynjndaceselppo', # Replace with your Gmail password or app-specific password
+  authentication: 'plain',
+  enable_starttls_auto: true
+}
+config.action_mailer.default_url_options = { host: 'shorturl-oazb.onrender.com',protocol: 'https' }
+
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
