71b4f76068557ba5cb7274c77eab399d27c9f836
Kiran K S <kiran.ks@geckolyst.com>
2023-12-21 15:18:25 +0530

    Unique mail is addded

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 50 }
 
 
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }  # , uniqueness: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
 
 
   validates :password, presence: true, length: { minimum: 6 }
@@ -20,5 +20,5 @@ class User < ApplicationRecord
 
   end
 
- 
+
 end
