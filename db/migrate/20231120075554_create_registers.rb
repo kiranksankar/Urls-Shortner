aee178e871e2ec506c61ad79f3908467efa019c3
Kiran K S <kiran.ks@geckolyst.com>
2023-12-22 16:07:10 +0530

    Main migration changes deleted

--- db/migrate/20231120075554_create_registers.rb
+++
@@ -1,5 +0,0 @@
-class CreateRegisters < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :registrations
-    end
-  end
--- db/migrate/20231129053005_drop_downloads.rb
+++
@@ -1,7 +0,0 @@
-class DropDownloads < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :fruits
-    
-
-  end
-end
