496b2de5ba4fa4857217454e8ddc9f23e15d84ee
Kiran K S <kiran.ks@geckolyst.com>
2023-12-22 12:44:54 +0530

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  # helper_method :current_user
+  helper_method :current_user
 
-  # def current_user
+  def current_user
 
-  #   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
 
-  # end
+  end
 
 end
